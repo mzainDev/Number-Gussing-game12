2f22f59cb203327ce4a619458977c6b6b2635fb0
Muhammad Zain <rubencallen689@gmail.com>
2024-04-18 16:33:47 +0500

    Number Guessing Game

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1)
+const answer = await inquirer.prompt([
+    {
+        name : 'UserGuessedNumber',
+        type : 'number',
+        message: 'please guess a number: ',
+    }
+])
+
+if(answer.UserGuessedNumber === randomNumber){
+    console.log("Congratulate you guessed right number");
+}else{
+    console.log('You guessed wrong number');
+}
